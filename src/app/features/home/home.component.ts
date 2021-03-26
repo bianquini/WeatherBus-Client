2e13e1b1881529424e5a1fed414b25ab09d6206f
Iago Bianquini <iagobianquini@gmail.com>
2021-03-25 21:20:35 -0300

    Adicionado mapa OpenLayers ao projeto

---
+++ src/app/features/home/home.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+
+import Map from 'ol/Map';
+import View from 'ol/View';
+import VectorLayer from 'ol/layer/Vector';
+import Style from 'ol/style/Style';
+import Icon from 'ol/style/Icon';
+import OSM from 'ol/source/OSM';
+import * as olProj from 'ol/proj';
+import TileLayer from 'ol/layer/Tile';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+  title = 'WeatherBus';
+  showFiller = false;
+  map: Map | undefined;
+
+  ngOnInit(){
+    this.map = new Map({
+      target: 'hotel_map',
+      layers: [
+        new TileLayer({
+          source: new OSM()
+        })
+      ],
+      view: new View({
+        center: olProj.fromLonLat([-87.65708976439701,41.85950283525066]),
+        zoom: 11
+      })
+    });
+  }
+
+}
